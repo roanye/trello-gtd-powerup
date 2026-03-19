6f78b4ca690d6b9c37344f3e0089fd2c8aab852b
roanye <roanpyeh@gmail.com>
2026-03-19 16:40:27 -0700

    config: add trello api key

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 /* global */
 // Replace YOUR_APP_KEY with the API key from https://trello.com/power-ups/admin
 var GTD_CONFIG = {
-  appKey: 'YOUR_APP_KEY'
+  appKey: '33461ff6337d737778dd27fe98a334cd'
 };
